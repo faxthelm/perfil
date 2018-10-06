33e09768d88f8a6cb1a222594790fc150eba2446
faxthelm <fernando.axthelm@usp.br>
2018-10-06 14:09:01 -0300

    commit inicial

---
+++ src/main/java/br/com/enjoei2/perfil/PerfilApplication.java
@@ -0,0 +1,12 @@
+package br.com.enjoei2.perfil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfilApplication.class, args);
+	}
+}
