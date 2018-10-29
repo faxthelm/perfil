902857ae1ad79caba2d6e2de0e6b092c99d782f0
Umaigenomu <handurimu@gmail.com>
2018-10-28 22:02:27 -0200

    removal of unnecessary annotations at application.java

--- src/main/java/br/com/enjoei2/perfil/Application.java
+++ src/main/java/br/com/enjoei2/perfil/Application.java
@@ -1,15 +1,11 @@
 package br.com.enjoei2.perfil;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
